26d7e2eda788c1b39ee1f2d96aeaad9a1132b5f2
Ilya Fedorov <74106915+MrPelmeshik@users.noreply.github.com>
2023-07-21 10:32:02 +0500

    init

--- src/front/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './variables.css'
-
-
-export const App = () => {
-  return <>
-    Hello, world!
-  </>
-}
---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import appStyle from './App.module.css';
+
+export const App = () => {
+  return <div className={appStyle.app}>
+    Hello, world!
+  </div>
+}
